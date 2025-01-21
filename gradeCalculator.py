30d5d3b5f86942adb248f1b2ed92733667c85104
Morgan <morgan.williams@vu.com>
2025-01-20 22:02:01 -0600

    elif

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,8 +2,22 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-
-    hrs = float(input("Enter score:"))
+try:
+    score = float(input("Enter score:"))
+    if score >= 0.9 and score <= 1.0:
+        print("A")
+    elif score >= 0.8 and score <= 0.9:
+        print("B")
+    elif score >= 0.7 and score <= 0.8:
+        print("C")
+    elif score >= 0.6 and score <= 0.7:
+        print("D")
+    elif score < 0.6 and score <= 0.6:
+        print("F")
+    else:
+        print("Bad score")
+except:
+    print("Bad score")
 
     # end assignment
 
